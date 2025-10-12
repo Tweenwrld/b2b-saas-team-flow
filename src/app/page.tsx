baadbffb613418f04a87954e1ac14f16b26bd9ec
Tweenwrld <154606522+Tweenwrld@users.noreply.github.com>
2025-10-12 23:36:56 +0300

    feat: step-1-shadcn-ui

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { ThemeToggle } from "@/components/ui/theme-toggle";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1> Hello world</h1>
+      <ThemeToggle />
+    </div>
+  );
+}

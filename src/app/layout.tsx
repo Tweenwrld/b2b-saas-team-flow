5b4ebf11c12c7877a43b589b76ded21efe7d8848
Tweenwrld <154606522+Tweenwrld@users.noreply.github.com>
2025-11-29 21:07:13 +0300

    chore: add CSS type declarations, integrate AuthProvider, and set up API routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/lib/theme-provider";
+import { AuthProvider } from "@/components/ui/AuthProvider";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +26,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+   <AuthProvider>
+     <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
@@ -37,5 +40,6 @@ export default function RootLayout({
         {children}
       </body>
     </html>
+   </AuthProvider>
   );
 }
